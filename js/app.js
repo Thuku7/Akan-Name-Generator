f32959018e59b43667a532084fbae42f9f62760b
Thuku7 <robethuku11@gmail.com>
2021-12-14 00:44:27 +0300

    will add user validation on their input

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 const form = document.getElementById("form")
 let maleNames =  ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 let femaleNames =  ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+
+document.getElementById("form").addEventListener("submit", function(e) {
+  e.preventDefault()
+
+
+let userDay =document.getElementById("day").value
+let userMonth =document.getElementById("month").value
+let userYear =document.getElementById("year").value
+
+
+let male = document.getElementById("male")
+let female = document.getElementById("female")
+
+
+//alert functions
+function alertUser() {
+  alert("Enter Valid Date")
+}
+
+function alertMonth() {
+  alert("Enter a valid Month")
+}
+
+function alertUserYear {
+  alert ("enter valid Year")
+}
