4e2c1646dfa5dece0f07b7e2fc61e9c46df9f291
Thuku7 <robethuku11@gmail.com>
2021-12-13 23:33:34 +0300

    link the pages to Javascript and add alert messages

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("form");
+
+maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+//alert prompts or user validation
+
+function alertUser {
+  alert(`Enter valid Date`)
+};
+
+function alertUser {
+  alert(`Enter valid Month`)
+};
+
+function alertUser {
+  alert(`Enter valid year`)
+};
+
+//engaging the form
+document.getElementById("form").addEventListener("submit", function(e) {
+  e.preventDefault()
